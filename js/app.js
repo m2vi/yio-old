279be632cb71515e4c2668ed474bdca4f299efec
m2v <69117810+iuvoJS@users.noreply.github.com>
2021-01-05 20:46:03 +0100

    commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  preload.stop(250);
+  preload.stop(1250);
   AOS.init({
     delay: 50, // values from 0 to 3000, with step 50ms
     duration: 1000, // values from 0 to 3000, with step 50ms
